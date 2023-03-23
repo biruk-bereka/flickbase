cea7fa9c0dd74114ed71a162454d4200b0330a30
biruk-bereka <birukbeletech1212@gmail.com>
2023-03-23 20:19:56 +0300

    Setup project with express an d creat react app

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, ()=> {
+    console.log(`Server running on port ${port}`);
+})
